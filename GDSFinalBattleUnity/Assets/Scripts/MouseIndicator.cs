851f7fa4ce010884b654d224db91e50c65293410
RafalWilkowski <kaizen.avc@gmail.com>
2020-05-15 20:18:17 +0200

    Add directional splash

---
+++ GDSFinalBattleUnity/Assets/Scripts/MouseIndicator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseIndicator : MonoBehaviour
+{
+
+    private MouseDetector _mouseDetector;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        _mouseDetector = FindObjectOfType<MouseDetector>();
+        _mouseDetector.OnMouseMove += UpdatePosition;
+    }
+
+    private void UpdatePosition(Vector3 position,Vector3 normal)
+    {
+        Vector3 newPosition = new Vector3(position.x, position.y, position.z);
+        transform.position = position;
+        transform.rotation = Quaternion.LookRotation(normal);
+        
+    }
+
+    private void OnDestroy()
+    {
+        if (_mouseDetector) _mouseDetector.OnMouseMove -= UpdatePosition;
+    }
+}
