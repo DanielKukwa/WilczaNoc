93d385908eb8452bb3b5469a8b533e9f7fd270f2
RafalWilkowski <kaizen.avc@gmail.com>
2020-05-31 10:39:42 +0200

    Add lights, clean project

--- GDSFinalBattleUnity/Assets/Scripts/CharacterCombat.cs
+++ GDSFinalBattleUnity/Assets/Scripts/CharacterCombat.cs
@@ -22,7 +22,7 @@ public class CharacterCombat : MonoBehaviour
     void Start()
     {
         myStats = GetComponent<CharacterStats>();
-        var blood = Resources.Load("Prefabs/BloodSplashV3");
+        var blood = Resources.Load("Prefabs/Blood/BloodSplashV3");
         bloodSplash = blood as GameObject;
     }
 
