fc48c2238bc7191585243bf84dadf2af2630af7c
RafalWilkowski <kaizen.avc@gmail.com>
2020-06-13 22:08:08 +0200

    Add blood paddle direction

--- GDSFinalBattleUnity/Assets/Scripts/VFX/BloodSplash.cs
+++ GDSFinalBattleUnity/Assets/Scripts/VFX/BloodSplash.cs
@@ -8,7 +8,8 @@ public class BloodSplash : MonoBehaviour
     private List<ParticleCollisionEvent> collisionEvents;
     private static int _bloodCounter = 0;
 
-    [SerializeField] private GameObject _bloodPaddlePrefab;
+    [SerializeField] private GameObject _bloodPaddlePrefabGround;
+    [SerializeField] private GameObject _bloodPaddlePrefabOther;
 
     void Start()
     {
@@ -18,25 +19,53 @@ public class BloodSplash : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log("particle hits");
         int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
 
         Collider collider = other.GetComponent<Collider>();
-        if (collider != null) Debug.Log("smtg");
 
         int i = 0;
         while (i < numCollisionEvents)
         {
             if (collider)
             {
-                Vector3 pos = collisionEvents[i].intersection;
-                Vector3 dir = collisionEvents[i].normal;
-                float offset = Random.RandomRange(0.01f, 0.1f);
-                GameObject bp = Instantiate(_bloodPaddlePrefab, pos ,Quaternion.LookRotation(dir));
-                float bloodSpeed = collisionEvents[i].velocity.magnitude;
-                Debug.Log("Speed:" + bloodSpeed);
+                Vector3 hitPoint = collisionEvents[i].intersection;
+                Vector3 planeNormal = collisionEvents[i].normal;
+
+                Vector3 dirOnPlane = Vector3.ProjectOnPlane(collisionEvents[i].velocity, planeNormal);
+                // normalize direction on plane vector
+                dirOnPlane = dirOnPlane.normalized;
+
+                Debug.Log(planeNormal.x + " | " + planeNormal.y + " | " + planeNormal.z);
+                if (planeNormal.y >= 0.99f)
+                {
+                    GameObject bloodPaddle = Instantiate(_bloodPaddlePrefabGround, hitPoint, Quaternion.LookRotation(dirOnPlane));
+                }
+                else
+                {
+                    GameObject bloodPaddle = Instantiate(_bloodPaddlePrefabOther, hitPoint, Quaternion.LookRotation(planeNormal));
+                    // get blood sprte vector up
+                    Vector3 bloodUp = bloodPaddle.transform.up;
+
+                    // angle between bloodUp and direction on plane
+                    float angle = Mathf.Acos(Vector3.Dot(bloodUp, dirOnPlane) / Vector3.Distance(Vector3.zero, bloodUp) * Vector3.Distance(Vector3.zero, dirOnPlane));
+
+                    // change to degress
+                    angle = angle * 180 / Mathf.PI;
+
+                    // check if direction dir on plane is on front or behind blood up vector
+                    if (Vector3.Cross(bloodUp, dirOnPlane).z >= 0)
+                    {
+                        angle = 360 - angle;
+                    }
+
+                    Debug.Log(angle);
+
+                    bloodPaddle.transform.Rotate(0, 0, angle);
+
+                }
             }                    
             i++;
         }
     }
+
 }
