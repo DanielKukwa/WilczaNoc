8596f70edda64b7f2cd2c1b89bbc1b612c2aeade
RafalWilkowski <kaizen.avc@gmail.com>
2020-06-15 22:13:01 +0200

    Add area Howl

---
+++ GDSFinalBattleUnity/Assets/HowlArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowlArea : MonoBehaviour
+{
+    private AudioSource _audio;
+    private bool played = false;
+    private MeshRenderer _mesh;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        _mesh = GetComponent<MeshRenderer>();
+        _mesh.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.tag == "Player")
+        {
+            if(!played)
+            {
+                _audio.Play();
+                played = true;              
+            }
+            
+        }
+    }
+
+}
