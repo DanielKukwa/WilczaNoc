8c07f6e4a3d8f734abfe265004f08c56dbf59f0c
RafalWilkowski <kaizen.avc@gmail.com>
2020-06-20 23:06:22 +0200

    Add Text Mesh Pro, Add narrative event

---
+++ GDSFinalBattleUnity/Assets/Scripts/ColliderBroadcast.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ColliderBroadcast : MonoBehaviour
+{
+    public event Action<Collider> OnEnterTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnEnterTrigger?.Invoke(other);
+    }
+}
