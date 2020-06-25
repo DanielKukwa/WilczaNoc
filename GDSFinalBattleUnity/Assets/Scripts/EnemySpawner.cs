aa8002b1e91938360dc1f8bad67217d630e5ddfc
RafalWilkowski <kaizen.avc@gmail.com>
2020-06-25 22:39:09 +0200

    Add Enemy Spawner and EnemyTriggger

---
+++ GDSFinalBattleUnity/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private MeshRenderer _meshRenderer;
+    [SerializeField] private GameObject _enemyToSpawn;
+    [SerializeField] private EventTrigger _eventTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshRenderer.enabled = false;
+
+        if (_eventTrigger)
+        {
+            _eventTrigger.OnEventTrigger += OnTriggerActive;
+        }
+    }
+
+    private void OnTriggerActive()
+    {
+        Instantiate(_enemyToSpawn, transform.position, Quaternion.identity);
+    }
+}
---
+++ GDSFinalBattleUnity/Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using System;
+
+public class EventTrigger : MonoBehaviour
+{
+    public event Action OnEventTrigger;
+
+    public void Triggered()
+    {
+        OnEventTrigger?.Invoke();
+    }
+}
