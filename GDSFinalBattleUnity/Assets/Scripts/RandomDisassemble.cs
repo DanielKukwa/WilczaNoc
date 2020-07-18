bb2a9c1c7e17c9f0056ad12c1508dbfadc8b1891
RafalWilkowski <kaizen.avc@gmail.com>
2020-07-18 19:36:25 +0200

    Add blood splash to body parts, add force

--- GDSFinalBattleUnity/Assets/Scripts/RandomDisassemble.cs
+++ GDSFinalBattleUnity/Assets/Scripts/RandomDisassemble.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class RandomDisassemble : MonoBehaviour
 {
+    [SerializeField] GameObject _particles;
+    private GameObject _player;
+    [SerializeField] private float _hitForce = 220f;
     [SerializeField] private float _timeToStop = 3f;
+
     private void Start()
     {
+        _player = PlayerManager.instance.player;
+
+        Rigidbody rb = GetComponentInChildren<Rigidbody>();
+        Vector3 direction = _player.transform.position - transform.position;
+        rb.AddForce(-direction * _hitForce, ForceMode.Impulse);
+
         CharacterJoint[] joints = GetComponentsInChildren<CharacterJoint>();
+
+        //int rand = Random.Range(0, joints.Length);
+        //CharacterJoint joint = joints[rand];
+        //Vector3 anchor = joint.anchor;
+
+        //GameObject bloodVein = Instantiate(_particles);
+        //bloodVein.transform.parent = joint.transform;
+        //bloodVein.transform.localPosition = joint.anchor;
+        //bloodVein.transform.localScale = Vector3.one;
+        //bloodVein.transform.rotation = Quaternion.LookRotation(Vector3.up);
+        //Destroy(joint.gameObject.GetComponent<CharacterJoint>());
         for(int i = 0; i < joints.Length; i++)
         {
             int rnd = Random.Range(0, 1 + 1);
             if (rnd == 1)
             {
+                CharacterJoint joint = joints[i];
+                Vector3 anchor = joint.anchor;
+
+                GameObject bloodVein = Instantiate(_particles);
+                bloodVein.transform.parent = joint.transform;
+                bloodVein.transform.localPosition = joint.anchor;
+                bloodVein.transform.localScale = Vector3.one;
+                bloodVein.transform.rotation = Quaternion.LookRotation(Vector3.up);
                 Destroy(joints[i].gameObject.GetComponent<CharacterJoint>());
                 
             }
