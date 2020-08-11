cb698c382cb7d355215e4ae825632ee5d2beccf2
RafalWilkowski <kaizen.avc@gmail.com>
2020-08-11 22:25:18 +0200

    Add timer

---
+++ GDSFinalBattleUnity/Assets/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float _timeElapsed;
+    Text _text;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeElapsed += Time.deltaTime;
+        int seconds = Mathf.FloorToInt(_timeElapsed % 60);
+        int minutes = Mathf.FloorToInt(_timeElapsed / 60);
+        _text.text = minutes + ":" + seconds;
+    }
+}
