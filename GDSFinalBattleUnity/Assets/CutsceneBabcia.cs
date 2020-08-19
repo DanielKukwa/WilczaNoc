d68247b7d92b8d5f99e8c7f9fa6f77d0be5aa0dd
RafalWilkowski <kaizen.avc@gmail.com>
2020-08-19 20:52:39 +0200

    Add BabciaEvent WIP

---
+++ GDSFinalBattleUnity/Assets/CutsceneBabcia.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneBabcia : MonoBehaviour
+{
+    private GameObject _light;
+
+    private void Start()
+    {
+        //GameObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
