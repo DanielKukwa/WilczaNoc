344fc0f4e35343471859352249c71eb1bd700957
RafalWilkowski <kaizen.avc@gmail.com>
2020-08-20 21:01:55 +0200

    Add Forester script

--- GDSFinalBattleUnity/Assets/Scripts/Forester.cs
+++ GDSFinalBattleUnity/Assets/Scripts/Forester.cs
@@ -2,7 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterStats))]
 public class Forester : Interactable
 {
-    
+    PlayerController playerController;
+    public bool isFirstAttack;
+    CharacterStats myStats;
+
+    private void Start()
+    {
+        myStats = GetComponent<CharacterStats>();
+        playerController = PlayerManager.instance.player.GetComponent<PlayerController>();
+    }
+    public override void Interact()
+    {
+        base.Interact();
+
+        CharacterCombat playerCombat = PlayerManager.instance.player.GetComponent<CharacterCombat>();
+
+        isFirstAttack = playerController.GetAttackInfo();
+
+        if (playerCombat != null && isFirstAttack == true)
+        {
+
+            playerCombat.Attack(myStats);
+
+        }
+        else if (playerCombat != null && isFirstAttack == false)
+        {
+            playerCombat.Attack2(myStats);
+        }
+
+    }
 }
