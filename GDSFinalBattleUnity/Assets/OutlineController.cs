7c7376db405974d38d1e1a01bfccbc94bf6e9247
Danny <danielkukwa@gmail.com>
2020-08-22 22:26:16 +0200

    Add outline shader part 1

---
+++ GDSFinalBattleUnity/Assets/OutlineController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineController : MonoBehaviour
+{
+    MeshRenderer renderer;
+    Camera cam;
+    public float dist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponentInChildren<MeshRenderer>();
+        renderer.enabled = false;
+        cam = Camera.FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtRay();
+        
+    }
+
+    private void LookAtRay()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, dist))
+        {
+            if (hit.collider.CompareTag("Interact"))
+            {
+                renderer.enabled = true;
+            }
+
+        }
+        else
+        {
+            renderer.enabled = false;
+        }
+
+
+    }
+}
