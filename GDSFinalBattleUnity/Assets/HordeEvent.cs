be38b897ae1d2862fb1dbc14d46a2e656ba4feaf
RafalWilkowski <kaizen.avc@gmail.com>
2020-08-25 23:15:19 +0200

    Horde event part 1

---
+++ GDSFinalBattleUnity/Assets/HordeEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HordeEvent : MonoBehaviour
+{
+    private EventTrigger _eventTrigger;
+    private AudioSource _audio;
+    [SerializeField] private EventTrigger[] _eventTriggers;
+    [SerializeField] private float _audioTime = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        _eventTrigger = GetComponent<EventTrigger>();
+
+        foreach (EventTrigger e in _eventTriggers)
+        {
+            e.OnEventTrigger += OnTriggerActive;
+        }
+    }
+
+    private void OnTriggerActive()
+    {
+        StartCoroutine(HordeAttackDelay());
+    }
+
+    private IEnumerator HordeAttackDelay()
+    {
+        _audio.Play();
+        yield return new WaitForSeconds(_audioTime);
+        _eventTrigger.Triggered();
+        
+    }
+}
