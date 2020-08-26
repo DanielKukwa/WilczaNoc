398d512ad377ec6d45fe4fe253a93733cdbe9aa7
RafalWilkowski <kaizen.avc@gmail.com>
2020-08-26 22:54:11 +0200

    Add Horde Event

---
+++ GDSFinalBattleUnity/Assets/Bridge.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    private Animator _anim;
+    private AudioSource _audio;
+    [SerializeField] private GameObject[] _lines;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_lines[0] && !_lines[1])
+        {
+            _anim.SetTrigger("Fall");
+            _audio.Play();
+            Destroy(this);
+        }
+    }
+}
