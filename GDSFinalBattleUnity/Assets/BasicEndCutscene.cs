e49e2fdfd446208adb93de4b82bfcdbc1388ed10
RafalWilkowski <kaizen.avc@gmail.com>
2020-08-28 20:45:36 +0200

    Change snow after event

--- GDSFinalBattleUnity/Assets/BasicEndCutscene.cs
+++ GDSFinalBattleUnity/Assets/BasicEndCutscene.cs
@@ -6,12 +6,17 @@ public class BasicEndCutscene : MonoBehaviour
 {
     [SerializeField] private float _blackScreenTime = 1f;
     [SerializeField] private Animator _blackScreen;
+    private GameObject _snowOld;
+    private GameObject _snowNew;
     private EventTrigger _eventTrigger;
     // Start is called before the first frame update
     private void Start()
     {
         _eventTrigger = GetComponentInChildren<EventTrigger>();
-        _eventTrigger.OnEventTrigger += OnTriggerActive;    
+        _eventTrigger.OnEventTrigger += OnTriggerActive;
+        _snowOld = GameObject.FindGameObjectWithTag("SnowOld");
+        _snowNew = GameObject.FindGameObjectWithTag("SnowNew");
+        _snowNew.SetActive(false);
     }
 
   
@@ -25,6 +30,8 @@ public class BasicEndCutscene : MonoBehaviour
     IEnumerator FadeOutAndIn()
     {
         yield return new WaitForSeconds(_blackScreenTime);
+        _snowNew.SetActive(true);
+        _snowOld.SetActive(false);
         _blackScreen.SetTrigger("FadeIn");
     }
 
