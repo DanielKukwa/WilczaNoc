89d596b0b97dfcb48f6e4728da624f2a39177f59
RafalWilkowski <kaizen.avc@gmail.com>
2020-08-30 15:39:42 +0200

    Add Cave Event

---
+++ GDSFinalBattleUnity/Assets/MovePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] EventTrigger _eventTrigger;
+    [SerializeField] Transform _destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _eventTrigger.OnEventTrigger += MovePlayerToPoint;
+    }
+
+    // Update is called once per frame
+    private void MovePlayerToPoint()
+    {
+        PlayerManager.instance.player.transform.position = _destination.position;
+        NavMeshAgent agent = PlayerManager.instance.player.GetComponent<NavMeshAgent>();
+        agent.Warp(_destination.position);
+    }
+}
