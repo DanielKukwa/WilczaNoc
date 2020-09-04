e189842484f9a49dca31fe9be28b50dceb6c5b6c
RafalWilkowski <kaizen.avc@gmail.com>
2020-09-04 20:17:42 +0200

    Add dash skill unlocked

---
+++ GDSFinalBattleUnity/Assets/TurnOnDash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnDash : MonoBehaviour
+{
+    private EventTrigger _eventTrigger;
+
+    private void Start()
+    {
+        _eventTrigger = GetComponent<EventTrigger>();
+        _eventTrigger.OnEventTrigger += OnTriggerActive;
+    }
+
+    private void OnTriggerActive()
+    {
+        SpecialAbilities playerAbilities = PlayerManager.instance.player.GetComponent<SpecialAbilities>();
+        playerAbilities.DashEnabled = true;
+        playerAbilities.dashBar.gameObject.SetActive(true);
+
+    }
+}
