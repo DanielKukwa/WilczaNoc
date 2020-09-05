730bf4daa90ea3521002fc666809144609886c71
RafalWilkowski <kaizen.avc@gmail.com>
2020-09-05 16:40:56 +0200

    Fix PlayerMotor

--- GDSFinalBattleUnity/Assets/Scripts/Controllers/PlayerMotor.cs
+++ GDSFinalBattleUnity/Assets/Scripts/Controllers/PlayerMotor.cs
@@ -23,7 +23,7 @@ public class PlayerMotor : MonoBehaviour
 
     void Update()
     {
-        if (target != null && SecondAttack)
+        if (target != null && !SecondAttack)
         {
             agent.SetDestination(target.position);
             FaceTarget(); 
