f642b86013de1190952f3bc6f722d88879430b8f
Danny <danielkukwa@gmail.com>
2020-09-10 01:20:33 +0200

    Fix II outline

--- GDSFinalBattleUnity/Assets/OutlineController.cs
+++ GDSFinalBattleUnity/Assets/OutlineController.cs
@@ -6,6 +6,7 @@ public class OutlineController : MonoBehaviour
 {
     Knife.HDRPOutline.Core.OutlineObject outline;
     Knife.HDRPOutline.Core.OutlineObject[] outlinesList;
+    OutlineVisibility outVis;
 
     void Start()
     {
@@ -13,23 +14,38 @@ public class OutlineController : MonoBehaviour
         outline = GetComponentInChildren<Knife.HDRPOutline.Core.OutlineObject>();
         outlinesList = GetComponentsInChildren<Knife.HDRPOutline.Core.OutlineObject>();
         //outline.enabled = false;
+        outVis = GetComponentInChildren<OutlineVisibility>();
     }
 
     public void ShowOutline()
     {
-        foreach(Knife.HDRPOutline.Core.OutlineObject outline in outlinesList) 
-        {
-            outline.enabled = true;
-        }
+        if (outVis) { outVis.enabled = false; }
 
+        foreach (Knife.HDRPOutline.Core.OutlineObject outline in outlinesList)
+        {
+            
+            //outline.enabled = true;
+            outline.Color = new Color(1, 0, 0, 1f);
+            
+        }
+        //outlinesList[1].enabled = true;
+        //outlinesList[0].enabled = false;
+        
+       
     }
 
     public void HideOutline()
     {
+        
         foreach (Knife.HDRPOutline.Core.OutlineObject outline in outlinesList)
         {
-            outline.enabled = false;
+           
+            outline.Color = new Color(0, 0, 0, 0f);
+           
         }
+        //outlinesList[1].enabled = false;
+        //outlinesList[0].enabled = true;
+        if (outVis) { outVis.enabled = true; }
     }
 
 }
---
+++ GDSFinalBattleUnity/Assets/OutlineVisibility.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineVisibility : MonoBehaviour
+{
+    private GameObject player;
+    private float fullVisibility = 5f;
+    private float noVisibility = 15f;
+    private float alpha;
+    Knife.HDRPOutline.Core.OutlineObject outline;
+    Knife.HDRPOutline.Core.OutlineObject[] outlinesList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = PlayerManager.instance.player;
+        outline = GetComponentInChildren<Knife.HDRPOutline.Core.OutlineObject>();
+        outlinesList = GetComponentsInChildren<Knife.HDRPOutline.Core.OutlineObject>();
+        outline.Color = new Color(1, 1, 1, 0f);
+        //alpha = outline.material.color.a;
+        //alpha = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Knife.HDRPOutline.Core.OutlineObject outline in outlinesList)
+        {
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+            if (distance < fullVisibility) outline.Color = new Color(1, 1, 1, 1f);
+            else if (distance > noVisibility) outline.Color = new Color(1, 1, 1, 0f);
+            else
+            {
+                outline.Color = new Color(1, 1, 1, Mathf.Lerp(0, 1, (noVisibility - distance) / (noVisibility - fullVisibility)));
+            }
+        }
+        //outline.Material.color.a = alpha;
+    }
+}
