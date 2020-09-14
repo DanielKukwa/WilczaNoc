c0106e68cb918e59b4a02d5912fe4748fc8a481a
Danny <danielkukwa@gmail.com>
2020-09-14 23:39:54 +0200

    Add main menu scene, Add scene loader script

---
+++ GDSFinalBattleUnity/Assets/SceneLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void LoadGameScene()
+    {
+        if (Application.isEditor)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene("LevelDesignAnti", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Kamil_enviro", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Events", LoadSceneMode.Additive);
+            SceneManager.LoadScene("DanInteracts", LoadSceneMode.Additive);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void LoadCredits()
+    {
+
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void LoadGameOver()
+    {
+
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+    }
+}
