e30c9f18a6ade002879b002b2b2883a6b07232ae
RafalWilkowski <kaizen.avc@gmail.com>
2020-09-18 22:01:42 +0200

    WIP Horde Scene

--- GDSFinalBattleUnity/Assets/HordeEvent.cs
+++ GDSFinalBattleUnity/Assets/HordeEvent.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class HordeEvent : Cutscene
 {
-    [SerializeField] private bool _iconBase = false;
-    private EventTrigger _eventTrigger;
+    [SerializeField] private SelectecedInfo _selectedInfo;
     private AudioSource _audio;
     [SerializeField] private Transform _destination;
     [SerializeField] private float _destinationDistanceOffset = 2f;
@@ -26,77 +25,45 @@ public class HordeEvent : Cutscene
     {
         _spawner.gameObject.SetActive(false);
         _audio = GetComponent<AudioSource>();
-        _eventTrigger = GetComponent<EventTrigger>();
-        _camController = Camera.main.GetComponent<CameraController>();
-
-        if (_eventTriggers != null)
-        {
-            foreach (EventTrigger e in _eventTriggers)
-            {
-                e.OnEventTrigger += OnTriggerActive;
-            }
-        }
-        
+        _selectedInfo.OnClick += OnTriggerActive;
+        _camController = Camera.main.GetComponent<CameraController>();      
         base.Start();
     }
 
     private void OnTriggerActive()
     {
         StartCoroutine(GoToPoint());
-        if (!_iconBase)
-        {
-            StartEvent();
-        }
-        
+        StartEvent();       
     }
 
     private IEnumerator HordeAttackDelay()
     {
         _audio.Play();
-        if (_iconBase)
-        {
-            gameObject.tag = "WolfHowl";
-            yield return new WaitForSeconds(_audioTime);
-            _audio.PlayOneShot(_wolfSounds[0], 0.1f);
-            yield return new WaitForSeconds(1f);
-            _audio.PlayOneShot(_wolfSounds[1], 0.5f);
-            _eventTrigger.Triggered();
-            yield return new WaitForSeconds(2f);
-            
-        }
-        else
-        {
-            _spawner.SetActive(true);
-            yield return new WaitForSeconds(_audioTime);
-            _blackScreen.Play("BS_fadeout");
-            yield return new WaitForSeconds(_blackScreenTime);
-            // TRIGGER HORDE
-            //_eventTrigger.Triggered();
-            
 
-            
-            _camController.enabled = false;
-            _camLight.gameObject.SetActive(true);
-            // set  camer to position
-            Vector3 camStartposition = Camera.main.transform.position;
-            Camera.main.transform.position = _cameraDestination.position;
+        _spawner.SetActive(true);
+        yield return new WaitForSeconds(_audioTime);
+        _blackScreen.Play("BS_fadeout");
+        yield return new WaitForSeconds(_blackScreenTime);
+           
+        _camController.enabled = false;
+        _camLight.gameObject.SetActive(true);
+        // set  camer to position
+        Vector3 camStartposition = Camera.main.transform.position;
+        Camera.main.transform.position = _cameraDestination.position;
 
-            _blackScreen.Play("BS_fadein");
-            _audio.PlayOneShot(_wolfSounds[0], 0.1f);
-            yield return new WaitForSeconds(1f);
-            _audio.PlayOneShot(_wolfSounds[1], 0.5f);
-            yield return new WaitForSeconds(_camStayTime);
-            _blackScreen.Play("BS_fadeout");
-            yield return new WaitForSeconds(_blackScreenTime);
-            Camera.main.transform.position = _playerController.transform.position;
-            _blackScreen.Play("BS_fadein");
-            //_spawner.SetActive(false);
-            _camLight.gameObject.SetActive(false);
-            _camController.enabled = true;
-            Final();
-        }               
-
-        
+        _blackScreen.Play("BS_fadein");
+        _audio.PlayOneShot(_wolfSounds[0], 0.1f);
+        yield return new WaitForSeconds(1f);
+        _audio.PlayOneShot(_wolfSounds[1], 0.5f);
+        yield return new WaitForSeconds(_camStayTime);
+        _blackScreen.Play("BS_fadeout");
+        yield return new WaitForSeconds(_blackScreenTime);
+        Camera.main.transform.position = _playerController.transform.position;
+        _blackScreen.Play("BS_fadein");
+        //_spawner.SetActive(false);
+        _camLight.gameObject.SetActive(false);
+        _camController.enabled = true;
+        Final();   
     }
 
     IEnumerator GoToPoint()
